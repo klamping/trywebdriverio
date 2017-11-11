3fb02cebecd4463c343327221915252434db1c79
Kevin Lamping <klamping@gmail.com>
2017-11-10 19:04:41 -0500

    update share and sponsor styles

--- public/app.js
+++ public/app.js
@@ -76,49 +76,13 @@
       return response.text();
     }).then(function(id) {
       let shareUrl = new URL('/share/' + id, document.location);
+      let input = document.getElementById('sharable-url');
       document.getElementById('shareUrl').className = "show";
       document.getElementById('sharable-url').value = shareUrl.href;
-      history.pushState(null, null, shareUrl.href)
+      input.select();
+      history.pushState(null, null, shareUrl.href);
     });
   };
-
-
-  function parse_query_string(query) {
-    var vars = query.split("&");
-    var query_string = {};
-    for (var i = 0; i < vars.length; i++) {
-      var pair = vars[i].split("=");
-      // If first entry with this name
-      if (typeof query_string[pair[0]] === "undefined") {
-        query_string[pair[0]] = decodeURIComponent(pair[1]);
-        // If second entry with this name
-      } else if (typeof query_string[pair[0]] === "string") {
-        var arr = [query_string[pair[0]], decodeURIComponent(pair[1])];
-        query_string[pair[0]] = arr;
-        // If third or later entry with this name
-      } else {
-        query_string[pair[0]].push(decodeURIComponent(pair[1]));
-      }
-    }
-    return query_string;
-  }
-
-  var query = window.location.search.substring(1);
-  let shareId = parse_query_string(query).share;
-
-  if (shareId) {
-    let req = fetch("/load/" + shareId, {
-      method: "GET"
-    });
-
-    req.then(function(response) {
-      return response.json();
-    }).then(function(content) {
-      // load content in to areas
-      baseUrl.value = content.baseUrl;
-      editor.setValue(content.file)
-    });
-  }
 
   connect();
 })();
