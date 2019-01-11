a8f5fccddbd04c2b6c37d22b6e571d15f49bcd38
Kevin Lamping <klamping@gmail.com>
2019-01-11 14:58:01 -0600

    update default test

--- default-test.js
+++ default-test.js
@@ -1,24 +1,12 @@
 module.exports = {
   baseUrl: 'http://webdriver.io',
-  file: `var expect = require('chai').expect;
-describe('webdriver.io api page', function() {
-    it('should be able to filter for commands', function () {
-        browser.url('/api.html');
+  file: `const assert = require('assert');
 
-        // filtering property commands
-        $('.searchbar input').setValue('getT');
-
-        // get all results that are displayed
-        var results = $$('.commands.property a').filter(function (link) {
-            return link.isVisible();
-        });
-
-        // assert number of results
-        expect(results.length).to.be.equal(3);
-
-        // check out second result
-        results[1].click();
-        expect($('.doc h1').getText()).to.be.equal('GETTEXT');
+describe('webdriver.io page', () => {
+    it('should have the right title', () => {
+        browser.url('https://webdriver.io');
+        const title = browser.getTitle();
+        assert.equal(title, 'WebdriverIO · Next-gen WebDriver test framework for Node.js');
     });
 });`
 }
